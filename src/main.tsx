79dcf396e32fc8d609aa3753a9bbcb1d7b430973
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-21 14:05:48 -0500

    feat: adding router and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ItemsProvider from './context/itemsContext.jsx';
+import router from './router/main.tsx';
+
 import './assets/globalStyles.scss';
-import ItemsProvider from './context/items.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ItemsProvider>
-      <App />
+      <RouterProvider router={router} />
     </ItemsProvider>
   </React.StrictMode>
 );
---
+++ src/router/main.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react-refresh/only-export-components */
+import { lazy } from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+const SearchBoxView = lazy(() => import('@/views/SearchBoxView.tsx'));
+const SearchResultView = lazy(() => import('@/views/SearchResultView.tsx'));
+const ProductDetails = lazy(() => import('@/views/ProductDetails.tsx'));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<SearchBoxView />}>
+      <Route path="/items" element={<SearchResultView />} />
+      <Route path="/items/:id" element={<ProductDetails />} />
+    </Route>
+  )
+);
+
+export default router;
