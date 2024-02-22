7e441eb87e0726dc893bc4b6aa6d63156caf7dc0
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-21 21:33:11 -0500

    feat: protecting not found routes

--- src/router/main.tsx
+++ src/router/main.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from 'react-router-dom';
 
 const SearchBoxView = lazy(() => import('@/views/SearchBoxView.tsx'));
 const SearchResultView = lazy(() => import('@/views/SearchResultView.tsx'));
@@ -8,10 +8,13 @@ const ProductDetails = lazy(() => import('@/views/ProductDetails.tsx'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<SearchBoxView />}>
-      <Route path="/items" element={<SearchResultView />} />
-      <Route path="/items/:id" element={<ProductDetails />} />
-    </Route>
+    <>
+      <Route path="/" element={<SearchBoxView />}>
+        <Route path="/items" element={<SearchResultView />} />
+        <Route path="/items/:id" element={<ProductDetails />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </>
   )
 );
 
