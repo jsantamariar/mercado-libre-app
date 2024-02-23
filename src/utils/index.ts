15f7bfe14d55b6c95d0d1f2b8c19ed3b49fe55d6
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-23 14:55:44 -0500

    feat: adding formatItemPrice utils fn

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+export function formatItemPrice(amount: number) {
+  const priceString = amount.toString();
+
+  const mainPart = priceString.slice(0, -2);
+  const decimal = priceString.slice(-2);
+
+  const priceFormatted = parseInt(mainPart).toLocaleString('es-AR');
+
+  return { priceFormatted, decimal };
+}
