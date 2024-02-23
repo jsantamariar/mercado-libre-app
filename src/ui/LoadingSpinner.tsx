ae5471db8a221f254d209606fbf7649d3ca4e100
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-23 14:54:33 -0500

    feat: adding LoadingSpinner component to show when loading pages

---
+++ src/ui/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+export default function LoadingSpinner() {
+  return (
+    <div id="loading-screen" className="loading-screen">
+      <div className="wrapper">
+        <div className="spinner">
+          <div className="content">Loading...</div>
+        </div>
+      </div>
+    </div>
+  );
+}
