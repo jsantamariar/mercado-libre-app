abc6f2e1dde85dc61cfbf231129fa257ca5e857d
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-23 16:00:02 -0500

    fix: fixing ErrorBoundary screen styles and logic

--- src/ui/ErrorBoundaryML.tsx
+++ src/ui/ErrorBoundaryML.tsx
@@ -1,14 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function ErrorBoundaryML() {
+  const navigate = useNavigate();
+
   const handleReload = () => {
+    navigate('/');
     window.location.reload();
   };
   return (
     <div className="error-boundary-container">
-      <h1>Algo salió mal.</h1>
-      <p>Parece que ha ocurrido un error inesperado. Haz click en el botón para volver al inicio e inténtalo de nuevo</p>
-      <button onClick={handleReload} className="home-link">
-        Volver al inicio
-      </button>
+      <div className="error-boundary-description">
+        <h1>Algo salió mal.</h1>
+        <p>Parece que ha ocurrido un error inesperado. Haz click en el botón para volver al inicio e inténtalo de nuevo</p>
+        <button onClick={handleReload} className="home-link">
+          Volver al inicio
+        </button>
+      </div>
     </div>
   );
 }
