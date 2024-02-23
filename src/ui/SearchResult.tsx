3cdee1a1898285f2f93790c2ff7ca1c465320ae7
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-23 14:56:55 -0500

    fix: adding breadcrumbs and fixing navingation when clicking a product

--- src/ui/SearchResult.tsx
+++ src/ui/SearchResult.tsx
@@ -1,12 +1,22 @@
-import { Result } from '@/types';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ItemsContext } from '@/context/itemsContext';
+import ItemCard from './ItemCard';
+import Breadcrumb from './Breadcrumb';
 
-export default function SearchResult({ item }: { item?: Result }) {
+export default function SearchResult() {
+  const { items } = useContext(ItemsContext);
+
   return (
-    <div className="item-card-container">
-      <h2>
-        Search Result
-        {item?.title} - ${item?.price}
-      </h2>
+    <div className="search-results-container">
+      <div className="search-results">
+        <Breadcrumb categories={items.categories} />
+        {items.items.map((item) => (
+          <Link key={item.id} to={`/items/${item.id}`} className="search-result-item">
+            <ItemCard item={item} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
